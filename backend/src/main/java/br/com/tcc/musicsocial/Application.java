c0de0dd9e9aa025c9e16ffe91d7ebc96a65c7063
wagnerjl <wagner.jl@puccampinas.edu.br>
2017-03-02 15:09:41 -0300

    [TCC-65] Configuração para deploy em weblogic server 11g

--- backend/src/main/java/br/com/tcc/musicsocial/Application.java
+++ backend/src/main/java/br/com/tcc/musicsocial/Application.java
@@ -7,14 +7,22 @@ package br.com.tcc.musicsocial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 /**
  *
  * @author wagnerjl
  */
 @SpringBootApplication
-public class Application {
-    public static void main(String... args) {
-        SpringApplication.run(Application.class, args);
-    }
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+	public static void main(final String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 }
