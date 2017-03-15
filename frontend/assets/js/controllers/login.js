40de7b270714882595958a07281f083a60e005e6
Unknown <janainecris.jcvb@gmail.com>
2017-03-14 22:05:00 -0300

    [TCC-58] - Conexão com a API

--- frontend/assets/js/controllers/login.js
+++ frontend/assets/js/controllers/login.js
@@ -3,16 +3,44 @@
 /* Controllers */
 
 angular.module('app')
-    .controller('LoginCtrl', ['$scope', function($scope) {
+    .factory('apiLogin', function($http){
+        return {
+            getApi: function(usuario){
+                return $http({
+                    method:'POST',
+                    url:'http://192.198.90.26:82/musicsocial/usuario/login',
+                    headers : {
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        login: 'usuario.login',
+                        senha: 'usuario.senha'
+                    }
+                })
+            }
+        }
+    })
+
+    .controller('LoginCtrl', ['$scope', 'apiLogin', function($scope, apiLogin) {
 
     	$scope.finished = function() {
              $scope.register.passequal = ($scope.user.password == $scope.user.cpassword) ? false : true; 
             //alert("Wizard finished :)");
         }
 
-        $scope.validateLogin = function(user) { 
+        $scope.validateLogin = function(login) { 
             //alert("Login Success :)");
-            
+            apiLogin.getApi(login).then(function(result){
+                console.log(result);
+                console.log("oi");
+                if (result.data.message == "Sucesso!") {
+                    //redireciona
+                }
+                else {
+                    $scope.login.valid = false;   
+                }
+            })
+                        
         }
 
 
