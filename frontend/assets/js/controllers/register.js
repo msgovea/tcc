9173799bf1c21c99c3e6bc3b6bd7d4b2ddf49151
wagnerjl <wagner.jl@puccampinas.edu.br>
2017-03-17 01:02:25 -0300

    [TCC-49] [WEB] Cadastro via Sistema

--- frontend/assets/js/controllers/register.js
+++ frontend/assets/js/controllers/register.js
@@ -2,8 +2,38 @@
 
 /* Controllers */
 
-angular.module('app')
-    .controller('RegisterCtrl', ['$scope', function($scope) {
+angular.module('app').factory('apiRegister', function($http) {
+        return {
+            getApi: function(usuario) {
+                return $http({
+                    method: 'POST',
+                    url: 'http://192.198.90.26:82/musicsocial/usuario/cadastro',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        
+                        email: usuario.email.$viewValue,
+                        senha: usuario.password.$viewValue,
+                        nivelUsuario: {
+                            codigoNivel: 1
+                        },
+                        tipoConexao: {
+                            codigoTipoConexao: 1
+                        },
+                        nome: usuario.name.$viewValue,
+                        apelido: usuario.username.$viewValue,
+                        dataNascimento: '1995-10-10',
+                        cidade: 'Campinas',
+                        estado: 'SP',
+                        pais: 'Brasil'
+
+                    }
+                })
+            }
+        }
+    })
+    .controller('RegisterCtrl', ['$scope', 'apiRegister', function($scope, apiRegister) {
 
     	$scope.finished = function() {
              $scope.register.passequal = ($scope.user.password == $scope.user.cpassword) ? false : true; 
@@ -11,9 +41,19 @@ angular.module('app')
         }
 
         $scope.createAccount = function(user) { 
-            //alert("Account created :)")
+            apiRegister.getApi(user).then(function(result){
+                console.log(result);
+                console.log("oi");
+                if (result.data.message == "Sucesso!") {
+                    //redireciona
+                    console.log("sucesso");
+                    $scope.register.invalid = false;   
 
-            
+                }
+                else {
+                    $scope.register.invalid = true;   
+                }
+            })
         }
 
 
