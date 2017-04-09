2105d2a45522cd25f001028f72c1caf6cfc9714e
Mateus Sauer Govêa <mateus.sauer@gmail.com>
2017-04-09 20:11:58 -0300

    [TCC-141] [TCC-142] [MOB] Publicações

--- mobile/app/src/main/java/br/edu/puccamp/app/util/Strings.java
+++ mobile/app/src/main/java/br/edu/puccamp/app/util/Strings.java
@@ -4,11 +4,18 @@ package br.edu.puccamp.app.util;
  * Created by Mateus on 4/2/2017.
  */
 
-public interface API {
-    String URL        = "http://192.198.90.26:82/MusicSocial/";
+public interface Strings {
+
+    //API
+    String URL        = "http://192.198.90.26:82/musicsocial/";
 
     String REGISTER   = "usuario/cadastro";
     String LOGIN      = "usuario/login";
     String RECOVERY   = "usuario/recuperar";
+
+    //SHAREDPREFERENCE
+
+    String USUARIO = "USUARIO";
+
 
 }
