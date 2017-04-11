e338511f1476978882111f4de1913882915109e1
Janaine <janainecris.jcvb@gmail.com>
2017-04-10 21:20:27 -0300

    [TCC-166] - Criação de telas de cadastro de gosto musical

--- frontend/assets/js/controllers/confirmar-cadastro.js
+++ frontend/assets/js/controllers/confirmar-cadastro.js
@@ -3,9 +3,27 @@
 /* Controllers */
 
 angular.module('app')
-    // Chart controller 
-    .controller('ConfirmaCadastroCtrl', ['$scope', function($scope) {
-        $scope.modal = {};
+    .controller('ConfirmaCadastroCtrl', ['$scope', '$state','$http', '$window', /*'apiGostoMusical',*/ function($scope, $state, $http, $window /*, apiGostoMusical*/) {
+        //$scope.publicacoes = apiGostoMusical.getApi()
+        $scope.gostos = []
+        $http.get('assets/js/api/mock_gosto_musical.json').success(function(data) {
+            for(var i = 0; i < data.object.length; i++){
+                $scope.gostos[i] = data.object[i]; 
+            }
+            console.log($scope.gostos);
+        });
+
+        $scope.cadastrarGostos = function (){
+            for(var j = 0; j < $scope.gostos.length; j++){
+                if ($scope.gostos[j].selecionado){
+                    console.log($scope.gostos[j]);
+                //mandar para API para gravar
+                }
+            }
+        }
+
+
+       /* $scope.modal = {};
         $scope.modal.slideUp = "default";
         $scope.modal.stickUp = "default";
 
@@ -22,6 +40,5 @@ angular.module('app')
                     modalElem.children('.modal-dialog').addClass('modal-lg');
                 }
             }
-        };
-
+        };*/
     }]);
