2be2e35c3b4a0cce00e9d57e5d1a8d99ee983172
Mateus Sauer Govêa <mateus.sauer@gmail.com>
2017-05-16 20:42:34 -0300

    [TCC-158] Publicações

--- mobile/app/src/main/java/br/edu/puccamp/app/posts/QuestionsAdapter.java
+++ mobile/app/src/main/java/br/edu/puccamp/app/posts/QuestionsAdapter.java
@@ -2,7 +2,6 @@ package br.edu.puccamp.app.posts;
 
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
